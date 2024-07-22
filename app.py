6c23860c16b6a985d82169bd2aa19a817093b61f
albalopes <alba.lopes@ifrn.edu.br>
2024-07-22 23:20:06 +0000

    Exemplo com criação de tabela dinâmica a partir de dicionário

--- app.py
+++ app.py
@@ -9,3 +9,11 @@ def index():
 @app.route("/cardapio")
 def cardapio():
     return render_template('cardapio.html')
+
+@app.route('/avaliacoes')
+def avaliacoes():
+    dados = [{'cliente': 'Alba', 'nota': 3},
+             {'cliente': 'Maria', 'nota': 5},
+             {'cliente': 'João', 'nota': 2}
+            ]
+    return render_template('avaliacoes.html', dados=dados)
