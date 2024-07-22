179542e93ec4539ece879e234454a2d946e6b6ef
albalopes <alba.lopes@ifrn.edu.br>
2024-07-22 23:00:05 +0000

    Incluindo links no menu de navegação

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route("/cardapio")
+def cardapio():
+    return render_template('cardapio.html')
